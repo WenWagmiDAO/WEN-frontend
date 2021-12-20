7f78881c18537858bfdd6eb2f98a3cd2f6f0b1e2
Bart <bartvaader@gmail.com>
2021-12-20 22:20:56 +0000

    bonds change test1

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -22,6 +22,26 @@ export const dai = new StableBond({
   bondContractABI: DaiBondContract,
   networkAddrs: {
     [NetworkID.Mainnet]: {
+      bondAddress: "0x40159B7309Cd82Bf5e8eb118dCAbe4781dDE2F68",
+      reserveAddress: "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E",
+    },
+    [NetworkID.Testnet]: {
+      bondAddress: "0x1B6F86BC319e3B363aC5299c045Ae29D95d7A623",
+      reserveAddress: "0xEF6834b5a29D75a883406B19f3eEefbF87b5031A",
+    },
+  },
+});
+
+// Old DAI
+export const dai_old = new StableBond({
+  name: "dai_old",
+  displayName: "DAI (old)",
+  bondToken: "DAI",
+  isAvailable: { [NetworkID.Mainnet]: true },
+  bondIconSvg: DaiImg,
+  bondContractABI: DaiBondContract,
+  networkAddrs: {
+    [NetworkID.Mainnet]: {
       bondAddress: "0x7f4aea8a240a8f5c0e62eb387d1f929536e1b756",
       reserveAddress: "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E",
     },
@@ -68,7 +88,7 @@ export const eth = new CustomBond({
 });
 
 export const ohm_dai = new LPBond({
-  name: "exod_dai_lp",
+  name: "wen_dai_lp",
   displayName: "WEN-DAI LP",
   bondToken: "DAI",
   isAvailable: { [NetworkID.Mainnet]: true },
@@ -77,7 +97,30 @@ export const ohm_dai = new LPBond({
   reserveContract: ReserveOhmDaiContract,
   networkAddrs: {
     [NetworkID.Mainnet]: {
-      bondAddress: "0xc82514b8a3f4579abee1592eadd249d693a7213e",
+      bondAddress: "0x538633080bCa332BA557eabBaA4d4B1E153df166", 
+      reserveAddress: "0x0169721fdca4c1e201f9f44cec5fb1c0af356ae1",
+    },
+    [NetworkID.Testnet]: {
+      bondAddress: "",
+      reserveAddress: "",
+    },
+  },
+  lpUrl:
+    "https://spookyswap.finance/add/0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E/0x86D7BcCB91B1c5A01A7aD7D7D0eFC7106928c7F8",
+});
+
+// Old ohm_dai
+export const ohm_dai_old = new LPBond({
+  name: "wen_dai_lp_old",
+  displayName: "WEN-DAI LP",
+  bondToken: "DAI",
+  isAvailable: { [NetworkID.Mainnet]: true },
+  bondIconSvg: OhmDaiImg,
+  bondContractABI: BondOhmDaiContract,
+  reserveContract: ReserveOhmDaiContract,
+  networkAddrs: {
+    [NetworkID.Mainnet]: {
+      bondAddress: "0xc82514b8a3f4579abee1592eadd249d693a7213e", // if this is your bond address just paste in the new address
       reserveAddress: "0x0169721fdca4c1e201f9f44cec5fb1c0af356ae1",
     },
     [NetworkID.Testnet]: {
@@ -93,7 +136,7 @@ export const ohm_dai = new LPBond({
 // Is it a stableCoin bond? use `new StableBond`
 // Is it an LP Bond? use `new LPBond`
 // Add new bonds to this array!!
-export const allBonds = [dai, ohm_dai];
+export const allBonds = [dai, ohm_dai, dai_old, ohm_dai_old];
 // TODO (appleseed-expiredBonds): there may be a smarter way to refactor this
 export const allExpiredBonds: (StableBond | CustomBond | LPBond)[] = [];
 export const allBondsMap = allBonds.reduce((prevVal, bond) => {
